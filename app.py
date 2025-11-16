ec2264aaf1e6485e69f3990c91f6f64b227f2a4d
Anbu <anbumca05@gmail.com>
2025-11-16 20:46:54 +0530

    Indentation changes

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-# --- Health Check Endpoint ---
-@app.route('/health')
-def health_check():
-
 @app.route('/<username>')
 def get_user_gists(username):
     try:
